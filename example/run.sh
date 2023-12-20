1209dba8aaa14c344120ffcf32207aee037e7e09
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-12-20 22:19:21 +0100

    Update run.sh

--- example/run.sh
+++ example/run.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+echo "Installing libchdb..."
+if command -v yum >/dev/null; then
+    wget -q -O - https://github.com/metrico/metrico.github.io/raw/main/libchdb_installer.sh | sudo bash
+    sudo yum install -y libchdb
+fi
+if command -v apt >/dev/null; then
+    wget -q -O - https://github.com/metrico/metrico.github.io/raw/main/libchdb_installer.sh | sudo bash
+    sudo apt install libchdb
+fi
+
 echo "Compiling libchdb example..."
 clang example.c -o chdb_test -I../ -L../ -lchdb
 ldd chdb_test
