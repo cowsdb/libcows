0426175bb29e7735125bd1de2997a2593778480b
root <root@localhost.localdomain>
2023-12-20 22:09:06 +0100

    add examples

--- example/example.c
+++ example/example.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "chdb.h"
+#include "libchdb.h"
 
 int main()
 {
--- example/run.sh
+++ example/run.sh
@@ -2,8 +2,8 @@
 set -e
 
 echo "Compiling libchdb example..."
-clang example.c -o example -I../ -L../ -lchdb
-ldd example
+clang example.c -o chdb_test -I../ -L../ -lchdb
+ldd chdb_test
 
 echo "Executing libchdb example..."
-./example
+./chdb_test
